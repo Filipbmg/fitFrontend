4fc0be6a4e2270a40425be57261d1216efd8755c
Filip Gynthersen <filipg@live.dk>
2025-01-09 22:51:54 +0100

    cleanup

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,13 @@
 <script>
   import { Router, Route } from 'svelte-routing';
   import Login from "./pages/Login.svelte";
-  import PrivateRouteGuard from './components/PrivateRouteGuard.svelte';
+  import Home from './pages/Home.svelte';
 
 </script>
 
 <main>
 <Router>
-  <Route path="/" component={Login} />
-  <Route path="/Home">
-    <PrivateRouteGuard>
-      <Home />
-    </PrivateRouteGuard>
-  </Route>
-  <Route path="/notes/:id">
-    <PrivateRouteGuard>
-      <NoteDetails />
-    </PrivateRouteGuard>
-  </Route>    
+  <Route path="/" component={Login}/>
+  <Route path="/Home" component={Home}/>
 </Router>
 </main>
